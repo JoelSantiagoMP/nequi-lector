a41d10eb6cdbf00bd21aeb07fbb8e8206f948b26
JoelSantiagoMP <yessirrr1105@gmail.com>
2026-01-24 15:13:07 -0500

    Update MongoDB connection URI in DBManager

--- database.py
+++ database.py
@@ -3,8 +3,9 @@ from datetime import datetime
 
 class DBManager:
     def __init__(self):
-        # REEMPLAZA ESTA URL con la que te dé MongoDB Atlas
-        self.client = MongoClient("TU_URL_DE_MONGO")
+        # Tu URL real de MongoDB Atlas
+        self.uri = "mongodb+srv://yessirrr1105_db_user:NLwtXyEDfALAiluo@cluster0.bykatji.mongodb.net/?appName=Cluster0"
+        self.client = MongoClient(self.uri)
         self.db = self.client['nequi_database']
         self.collection = self.db['movimientos']
 
@@ -19,22 +20,17 @@ class DBManager:
         self.collection.insert_one(nuevo_doc)
 
     def obtener_ultimos_movimientos(self, limite=15):
-        # Buscamos y ordenamos por fecha/id de forma descendente
         docs = self.collection.find().sort("_id", -1).limit(limite)
-        # Convertimos al formato de lista que espera tu main.py
         return [[d['tipo'], d['monto'], d['fecha'], d['detalle']] for d in docs]
 
     def obtener_resumen_mensual(self):
         mes_actual = datetime.now().strftime("%Y-%m")
         movimientos = self.collection.find({"fecha": {"$regex": f"^{mes_actual}"}})
-        
         ingresos = 0.0
         egresos = 0.0
-        
         for doc in movimientos:
             if doc['tipo'] == 'Ingreso':
                 ingresos += doc['monto']
             else:
                 egresos += doc['monto']
-                
         return ingresos, egresos
